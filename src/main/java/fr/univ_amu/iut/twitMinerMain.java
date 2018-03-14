a69f7568ebc905ec7f27f69dc964d67f07c236a1
t16016200 <arthur.tamas@etu.univ-amu.fr$>
2018-03-14 10:13:05 +0100

    Debut Phase 2

--- src/main/java/fr/univ_amu/iut/twitMinerMain.java
+++ src/main/java/fr/univ_amu/iut/twitMinerMain.java
@@ -1,7 +1,32 @@
 package fr.univ_amu.iut;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class twitMinerMain {
     public static void main(String[] args) {
-
+        File dataFile = new File("/home/t16016200/Documents/document.txt");
+        try {
+            InputStream ips = new FileInputStream(dataFile);
+            InputStreamReader ipsr = new InputStreamReader(ips);
+            BufferedReader br = new BufferedReader(ipsr);
+            String ligne;
+            while ((ligne = br.readLine()) != null) {
+// recuperation de la ligne courante
+                System.out.println("Contenu de la ligne:" + ligne);
+// separation de la ligne avec le toke " " (espace)
+//
+                String token = " ";
+                StringTokenizer stringTokenizer = new StringTokenizer(ligne, token);
+// Parcours des tokens de la ligne
+                while (stringTokenizer.hasMoreElements()) {
+                    String element = (String) stringTokenizer.nextElement();
+                    System.out.println("Element : " + element);
+                }
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
 }
