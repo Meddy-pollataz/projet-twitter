4b8fd9a1ffabfba4b0e08319cb42bf49cc30b542
Billy <billy.tran@etu.univ-amu.fr>
2018-04-15 22:36:16 +0200

    commit

--- src/main/java/fr/univ_amu/iut/twitMinerMain.java
+++ src/main/java/fr/univ_amu/iut/twitMinerMain.java
@@ -2,8 +2,8 @@ package fr.univ_amu.iut;
 
 public class twitMinerMain {
     public static void main(String[] args) {
-        lift myLift = new lift();
-        myLift.generateLiftFile("testminConf.out",1.2);
+        converterToCSV myConverter = new converterToCSV();
+        myConverter.convert("test.trans");
 
     }
 }
